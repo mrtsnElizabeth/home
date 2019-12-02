9090849c0bd11efbba271ef64bd75cd245afba12
Elizabeth <elizabeth.martsyn@outlook.com>
2019-12-02 23:11:50 +0200

    second lesson homework

---
+++ les2/example1.py
@@ -0,0 +1,5 @@
+test = True
+result = 'Test is True' if test else 'Test is False'
+
+test = True
+print ('ttt' if test else 'fff')
---
+++ les2/if-elif-else.py
@@ -0,0 +1,9 @@
+print ("How many bananas you have?")
+answer = int(input())
+
+if answer == 1:
+	print ("You can eat it!")
+elif answer > 1:
+	print ("Give me one, please.")
+else:
+	print ("I can't understand you...")
